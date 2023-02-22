a22d9ca95bea2c7d21cd5c25e2c29f050b054d29
Juan <johnyromerojrg@gmail.com>
2023-02-22 20:30:42 +0100

    Creación del rol Company

---
+++ src/main/java/acme/roles/Company.java
@@ -0,0 +1,53 @@
+/*
+ * Consumer.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Company extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			name;
+
+	@NotBlank
+	@Length(max = 26)
+	protected Integer			VAT_number;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			summary;
+
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
