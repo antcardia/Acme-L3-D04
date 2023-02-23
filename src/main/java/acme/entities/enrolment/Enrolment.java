751d7237d7a4ff0717854ab0836ac5a296b6f604
Eduardo Pizarro <edupriv2002@gmail.com>
2023-02-23 11:47:05 +0100

    Entidad enrolment creada

---
+++ src/main/java/acme/entities/enrolment/Enrolment.java
@@ -0,0 +1,39 @@
+
+package acme.entities.enrolment;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Enrolment extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			motivation;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			goals;
+
+	@Min(0)
+	protected Integer			hours;
+
+}
