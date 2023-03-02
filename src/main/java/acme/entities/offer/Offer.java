6d0d51d01528f477ed021ef7f8bc45d4011e9160
Eduardo Pizarro <edupriv2002@gmail.com>
2023-03-02 11:50:23 +0100

    Creada la entidad offer

---
+++ src/main/java/acme/entities/offer/Offer.java
@@ -0,0 +1,44 @@
+
+package acme.entities.offer;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Offer extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@Past
+	protected Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			heading;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			summary;
+
+	protected Date				availabilityPeriod;
+
+	@Min(0)
+	protected Double			price;
+
+	@URL
+	protected String			link;
+
+}
