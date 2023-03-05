6c6a3636322d45f407c5fe6e27d58e69c9222a49
Eduardo Pizarro <edupriv2002@gmail.com>
2023-03-05 18:29:53 +0100

    Solucion a errores y entidad studentDashboard

--- src/main/java/acme/entities/enrolment/Enrolment.java
+++ src/main/java/acme/entities/enrolment/Enrolment.java
@@ -1,10 +1,12 @@
 
 package acme.entities.enrolment;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
@@ -33,6 +35,9 @@ public class Enrolment extends AbstractEntity {
 	@Length(max = 100)
 	protected String			goals;
 
-	@Min(0)
-	protected Integer			hours;
+	@NotNull
+	protected Date				startTime;
+
+	@NotNull
+	protected Date				finishTime;
 }
