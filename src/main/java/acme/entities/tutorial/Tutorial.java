b2efda0882d0c17280782b907ca93fc3e2d7d484
davra02 <davidreyesales@gmail.com>
2023-03-07 10:51:44 +0100

    Fixed tutorial.

--- src/main/java/acme/entities/tutorial/Tutorial.java
+++ src/main/java/acme/entities/tutorial/Tutorial.java
@@ -1,13 +1,17 @@
 
 package acme.entities.tutorial;
 
+import java.util.Set;
+
 import javax.persistence.Column;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.entities.tutorial.Session;
 import acme.framework.data.AbstractEntity;
 
 public class Tutorial extends AbstractEntity {
@@ -18,6 +22,10 @@ public class Tutorial extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
+	@NotBlank
+	@OneToMany
+	protected Set<Session> session;
+	
 	@NotBlank(message = "A code must be specified")
 	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}", message = "Only images of type JPEG or GIF are supported.")
 	@Column(unique = true)
