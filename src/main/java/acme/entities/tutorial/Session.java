ef1ec94e7a5479444f63442a547e271517820320
David Reyes Alés <davidreyesales@gmail.com>
2023-03-09 21:21:00 +0100

    Session terminado

--- src/main/java/acme/entities/tutorial/Session.java
+++ src/main/java/acme/entities/tutorial/Session.java
@@ -1,14 +1,19 @@
 
 package acme.entities.tutorial;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.URL;
 
+import acme.datatypes.Nature;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,18 +30,26 @@ public class Session extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank(message = "Title must be specified")
-	@Size(max = 76, message = "Title must be shorter than 76 characters")
+	@Size(max = 75, message = "Title must be shorter than 76 characters")
 	protected String			title;
 
 	@NotBlank(message = "A summary must be specified")
-	@Size(max = 101, message = "The summary must be shorter than 101 characters")
+	@Size(max = 100, message = "The summary must be shorter than 101 characters")
 	protected String			summary;
 
-	protected SessionType		sessionType;
+	protected Nature			sessionType;
 
-	@Min(1)
-	@Max(5)
-	protected Integer			duration;
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				start;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	// Esperaremos a las restricciones custom para decir al sistema que
+	// la la fecha final va después de la fecha de inicio, asía como algunas
+	// otras restricciones
+	protected Date				end;
 
 	@URL
 	protected String			furtherInformation;
