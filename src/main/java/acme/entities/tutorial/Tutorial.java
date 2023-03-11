eb532614cd3dba29c77897c28cd1f9a1bdd37da3
Eduardo Pizarro <87647202+Domi-ATLAS@users.noreply.github.com>
2023-03-11 21:01:22 +0100

    Update Tutorial.java

--- src/main/java/acme/entities/tutorial/Tutorial.java
+++ src/main/java/acme/entities/tutorial/Tutorial.java
@@ -18,7 +18,7 @@ public class Tutorial extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}\\d{3}$")
 	@Column(unique = true)
 	protected String			code;
 
