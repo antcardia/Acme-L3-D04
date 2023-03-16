043266981020f8e2077d9a380207b73d3ccbc96f
juanrg8 <johnyromerojrg@gmail.com>
2023-03-16 12:33:42 +0100

    Agregada la relacion OneToMany hacia course

--- src/main/java/acme/entities/practicum/Practicum.java
+++ src/main/java/acme/entities/practicum/Practicum.java
@@ -3,12 +3,16 @@ package acme.entities.practicum;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.entities.courses.Course;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -48,4 +52,8 @@ public class Practicum extends AbstractEntity {
 
 	// Relationships ---------------------------------------------------------
 
+	@ManyToOne(optional = false)
+	@NotNull
+	@Valid
+	protected Course			course;
 }
