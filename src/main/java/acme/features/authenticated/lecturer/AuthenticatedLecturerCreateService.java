26acdc2c545cf65f1cb1b337a2ad6f093d3ea528
antcardia <antcardia4@alum.us.es>
2023-03-30 11:16:54 +0200

    Convertirte en profesor completado

---
+++ src/main/java/acme/features/authenticated/lecturer/AuthenticatedLecturerCreateService.java
@@ -0,0 +1,103 @@
+/*
+ * AuthenticatedProviderCreateService.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.authenticated.lecturer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.accounts.Principal;
+import acme.framework.components.accounts.UserAccount;
+import acme.framework.components.models.Tuple;
+import acme.framework.controllers.HttpMethod;
+import acme.framework.helpers.PrincipalHelper;
+import acme.framework.services.AbstractService;
+import acme.roles.Lecturer;
+
+@Service
+public class AuthenticatedLecturerCreateService extends AbstractService<Authenticated, Lecturer> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuthenticatedLecturerRepository repository;
+
+	// AbstractService<Authenticated, Provider> ---------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+
+		status = !super.getRequest().getPrincipal().hasRole(Lecturer.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void load() {
+		Lecturer object;
+		Principal principal;
+		int userAccountId;
+		UserAccount userAccount;
+
+		principal = super.getRequest().getPrincipal();
+		userAccountId = principal.getAccountId();
+		userAccount = this.repository.findOneUserAccountById(userAccountId);
+
+		object = new Lecturer();
+		object.setUserAccount(userAccount);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Lecturer object) {
+		assert object != null;
+
+		super.bind(object, "almaMater", "resume", "listOfQualifications", "furtherInformation");
+	}
+
+	@Override
+	public void validate(final Lecturer object) {
+		assert object != null;
+	}
+
+	@Override
+	public void perform(final Lecturer object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Lecturer object) {
+		Tuple tuple;
+
+		tuple = super.unbind(object, "almaMater", "resume", "listOfQualifications", "furtherInformation");
+
+		super.getResponse().setData(tuple);
+	}
+
+	@Override
+	public void onSuccess() {
+		if (super.getRequest().getMethod().equals(HttpMethod.POST))
+			PrincipalHelper.handleUpdate();
+	}
+
+}
