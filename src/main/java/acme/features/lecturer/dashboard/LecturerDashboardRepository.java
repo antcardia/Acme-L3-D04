0238b410fc9e8cac8db69132f47d7ef827bdd65d
antcardia <antcardia4@alum.us.es>
2023-04-10 08:53:21 +0200

    Avances en LecturerDashboard

--- src/main/java/acme/features/lecturer/dashboard/LecturerDashboardRepository.java
+++ src/main/java/acme/features/lecturer/dashboard/LecturerDashboardRepository.java
@@ -12,41 +12,41 @@
 
 package acme.features.lecturer.dashboard;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.datatypes.Nature;
+import acme.framework.components.accounts.UserAccount;
 import acme.framework.repositories.AbstractRepository;
+import acme.roles.Lecturer;
 
 @Repository
 public interface LecturerDashboardRepository extends AbstractRepository {
 
-	@Query("select count(l) from Lecture l where l.lectureType = acme.datatypes.Nature.THEORETICAL")
-	Integer totalTheoreticalLectures();
+	@Query("select count(distinct l) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.lecturer = :lecturer and l.lectureType = :nature")
+	Optional<Integer> totalLecturesByType(Lecturer lecturer, Nature nature);
 
-	@Query("select count(l) from Lecture l where l.lectureType = acme.datatypes.Nature.HANDS_ON")
-	Integer totalHandsOnLectures();
+	@Query("select avg(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.lecturer = :lecturer")
+	Optional<Double> averageLearningTimeOfLectures(Lecturer lecturer);
 
-	@Query("select avg(l.estimatedLearningTime) from Lecture l")
-	Double averageLearningTimeOfLectures();
+	@Query("select stddev(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.lecturer = :lecturer")
+	Optional<Double> deviationLearningTimeOfLectures(Lecturer lecturer);
 
-	@Query("select stdev(l.estimatedLearningTime) from Lecture l")
-	Double deviationLearningTimeOfLectures();
+	@Query("select min(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.lecturer = :lecturer")
+	Optional<Double> minimumLearningTimeOfLectures(Lecturer lecturer);
 
-	@Query("select min(l.estimatedLearningTime) from Lecture l")
-	Double minimumLearningTimeOfLectures();
+	@Query("select max(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.lecturer = :lecturer")
+	Optional<Double> maximumLearningTimeOfLectures(Lecturer lecturer);
 
-	@Query("select max(l.estimatedLearningTime) from Lecture l")
-	Double maximumLearningTimeOfLectures();
+	@Query("select sum(l.estimatedLearningTime) from Course c join LectureCourse lc on c = lc.course join Lecture l on lc.lecture = l where c.lecturer = :lecturer group by c")
+	Collection<Double> findManyEstimatedLearningTimeByCourse(Lecturer lecturer);
 
-	@Query("select avg(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.id = 1")
-	Double averageLearningTimeOfCourses();
+	@Query("select l from Lecturer l where l.userAccount.id = :id")
+	Lecturer findOneLecturerByUserAccountId(int id);
 
-	@Query("select stdev(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.id = 1")
-	Double deviationLearningTimeOfCourses();
-
-	@Query("select min(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.id = 1")
-	Double minimumLearningTimeOfCourses();
-
-	@Query("select max(l.estimatedLearningTime) from Lecture l inner join LectureCourse lc on l = lc.lecture inner join Course c on lc.course = c where c.id = 1")
-	Double maximumLearningTimeOfCourses();
+	@Query("select ua from UserAccount ua where ua.id = :id")
+	UserAccount findOneUserAccountById(int id);
 }
--- src/main/java/acme/features/lecturer/dashboard/LecturerDashboardShowService.java
+++ src/main/java/acme/features/lecturer/dashboard/LecturerDashboardShowService.java
@@ -12,9 +12,14 @@
 
 package acme.features.lecturer.dashboard;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.datatypes.Nature;
 import acme.forms.LecturerDashboard;
 import acme.framework.components.models.Tuple;
 import acme.framework.services.AbstractService;
@@ -43,23 +48,32 @@ public class LecturerDashboardShowService extends AbstractService<Lecturer, Lect
 
 	@Override
 	public void load() {
-		final LecturerDashboard dashboard;
-		final Double averageLearningTimeOfLectures;
+		final LecturerDashboard dashboard = new LecturerDashboard();
+		int userAccountId;
+		userAccountId = super.getRequest().getPrincipal().getAccountId();
+		final Lecturer lecturer = this.repository.findOneLecturerByUserAccountId(userAccountId);
+		final double avgLearningTimeLectures = this.repository.averageLearningTimeOfLectures(lecturer).orElse(0.0);
+		final double devLearningTimeLectures = this.repository.deviationLearningTimeOfLectures(lecturer).orElse(0.0);
+		final double minLearningTimeLectures = this.repository.minimumLearningTimeOfLectures(lecturer).orElse(0.0);
+		final double maxLearningTimeLectures = this.repository.maximumLearningTimeOfLectures(lecturer).orElse(0.0);
 
-		averageLearningTimeOfLectures = this.repository.averageLearningTimeOfLectures();
-		//		averageNumberOfApplicationsPerWorker = this.repository.averageNumberOfApplicationsPerWorker();
-		//		averageNumberOfJobsPerEmployer = this.repository.averageNumberOfJobsPerEmployer();
-		//		ratioOfPendingApplications = this.repository.ratioOfPendingApplications();
-		//		ratioOfAcceptedApplications = this.repository.ratioOfAcceptedApplications();
-		//		ratioOfRejectedApplications = this.repository.ratioOfRejectedApplications();
+		dashboard.setAverageLearningTimeOfLectures(avgLearningTimeLectures);
+		dashboard.setDeviationLearningTimeOfLectures(devLearningTimeLectures);
+		dashboard.setMinimumLearningTimeOfLectures(minLearningTimeLectures);
+		dashboard.setMaximumLearningTimeOfLectures(maxLearningTimeLectures);
 
-		dashboard = new LecturerDashboard();
-		dashboard.setAverageLearningTimeOfLectures(averageLearningTimeOfLectures);
-		//		dashboard.setAverageNumberOfApplicationsPerWorker(averageNumberOfApplicationsPerWorker);
-		//		dashboard.setAverageNumberOfJobsPerEmployer(averageNumberOfJobsPerEmployer);
-		//		dashboard.setRatioOfPendingApplications(ratioOfPendingApplications);
-		//		dashboard.setRatioOfAcceptedApplications(ratioOfAcceptedApplications);
-		//		dashboard.setRatioOfRejectedApplications(ratioOfRejectedApplications);
+		final Collection<Double> courseEstimatedLearningTime = this.repository.findManyEstimatedLearningTimeByCourse(lecturer);
+		dashboard.averageCalc(courseEstimatedLearningTime);
+		dashboard.deviationCalc(courseEstimatedLearningTime);
+		dashboard.minimumCalc(courseEstimatedLearningTime);
+		dashboard.maximumCalc(courseEstimatedLearningTime);
+
+		final Map<Nature, Integer> lecturesByType = new HashMap<Nature, Integer>();
+		final Integer handsOnLectures = this.repository.totalLecturesByType(lecturer, Nature.HANDS_ON).orElse(0);
+		final Integer theoreticalLectures = this.repository.totalLecturesByType(lecturer, Nature.THEORETICAL).orElse(0);
+		lecturesByType.put(Nature.HANDS_ON, handsOnLectures);
+		lecturesByType.put(Nature.THEORETICAL, theoreticalLectures);
+		dashboard.setTotalLecturesByType(lecturesByType);
 
 		super.getBuffer().setData(dashboard);
 	}
@@ -69,9 +83,11 @@ public class LecturerDashboardShowService extends AbstractService<Lecturer, Lect
 		Tuple tuple;
 
 		tuple = super.unbind(object, //
-			"averageLearningTimeOfLectures", "averageNumberOfApplicationsPerWorker", // 
-			"avegageNumberOfApplicationsPerEmployer", "ratioOfPendingApplications", //
-			"ratioOfRejectedApplications", "ratioOfAcceptedApplications");
+			"totalLecturesByType", "averageLearningTimeOfLectures", // 
+			"deviationLearningTimeOfLectures", "minimumLearningTimeOfLectures", //
+			"maximumLearningTimeOfLectures", "averageLearningTimeOfCourses", // 
+			"deviationLearningTimeOfCourses", "minimumLearningTimeOfCourses", // 
+			"maximumLearningTimeOfCourses");
 
 		super.getResponse().setData(tuple);
 	}
