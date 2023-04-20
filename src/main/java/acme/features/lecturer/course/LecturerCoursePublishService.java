03cfc1c2753c9c5d9feb0d8d53e1e8919213c0ee
antcardia <antcardia4@alum.us.es>
2023-04-20 19:57:07 +0200

    Documentación Student #1 actualizada

--- src/main/java/acme/features/lecturer/course/LecturerCoursePublishService.java
+++ src/main/java/acme/features/lecturer/course/LecturerCoursePublishService.java
@@ -99,9 +99,9 @@ public class LecturerCoursePublishService extends AbstractService<Lecturer, Cour
 
 		if (!super.getBuffer().getErrors().hasErrors("code")) {
 			Course existing;
-
+			final Course course = this.repository.findOneCourseById(object.getId());
 			existing = this.repository.findOneCourseByCode(object.getCode());
-			super.state(existing == null, "code", "lecturer.course.form.error.duplicated");
+			super.state(existing == null || course.equals(existing), "code", "lecturer.course.form.error.duplicated");
 		}
 
 		if (!super.getBuffer().getErrors().hasErrors("retailPrice"))
